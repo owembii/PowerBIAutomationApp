152d8dd62c93eca06ef2b3f318bbd5457de592a6
Jay Nayon <Jay.Nayon@amcsgroup.com>
2025-02-26 18:18:35 +0800

    Add FBConfigManager

---
+++ PowerBIAutomationApp/FBConfigManager.cs
@@ -0,0 +1,99 @@
+﻿using FigBytesUtility.Core;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBFunctionServiceBL.UtilityVault
+{
+    public static class FBConfigManager
+    {
+        private static readonly string strVaultURL;
+        static FBConfigurationManager _fBConfigurationManager;
+        private static readonly string strVaultAppReaderName;
+
+        static FBConfigManager()
+        {
+            strVaultURL = Environment.GetEnvironmentVariable("AzureKeyVaultURL");
+            strVaultAppReaderName = Environment.GetEnvironmentVariable("AzureKeyVaultReaderAppName");
+        }
+        public static string GetConfigurationSetting(int iOrganizationID, string strSecretName)
+        {
+            string strSecretValue = string.Empty;
+
+            try
+            {
+                _fBConfigurationManager = new FBConfigurationManager(iOrganizationID, strVaultURL, strVaultAppReaderName);
+                strSecretValue = _fBConfigurationManager.GetConfigurationSetting(strSecretName);
+            }
+            catch (Exception Ex)
+            {
+                throw;
+            }
+
+            return strSecretValue;
+        }
+        public static string GetConfigurationSettingByEnvironmentID(int iEnvironmentID, string strSecretName)
+        {
+            string strSecretValue = string.Empty;
+
+            try
+            {
+                _fBConfigurationManager = new FBConfigurationManager(iEnvironmentID, strVaultURL, strVaultAppReaderName);
+                strSecretValue = _fBConfigurationManager.GetConfigurationSetting(iEnvironmentID, strSecretName);
+            }
+            catch (Exception Ex)
+            {
+                throw;
+            }
+
+            return strSecretValue;
+        }
+        public static async Task<string> GetAccessToken()
+        {
+            string clientId = Environment.GetEnvironmentVariable(strVaultAppReaderName + "_AzureClientID");
+            string clientSecret = Environment.GetEnvironmentVariable(strVaultAppReaderName + "_AzureClientSecret"); ;
+            string tenantId = Environment.GetEnvironmentVariable(strVaultAppReaderName + "_AzureTenantID");
+
+            string authority = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token";
+            string resource = "https://analysis.windows.net/powerbi/api/.default"; // Power BI API resource
+
+            string strAcessToken = string.Empty;
+
+            try
+            {
+                var app = ConfidentialClientApplicationBuilder.Create(clientId)
+                .WithClientSecret(clientSecret)
+                .WithAuthority(new Uri(authority))
+                .Build();
+
+                var result = await app.AcquireTokenForClient(new[] { resource })
+                                  .ExecuteAsync();
+
+                strAcessToken = result.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            
+            return strAcessToken;
+        }
+        public static string Encryption(string strText)
+        {
+            string strDecryptText = string.Empty;
+            FigBytesUtility.Core.Utility utility = new FigBytesUtility.Core.Utility(strVaultURL, strVaultAppReaderName);
+            strDecryptText = utility.Encryption(strText);
+            return strDecryptText;
+        }
+        public static string Decryption(string strText)
+        {
+            string strDecryptText = string.Empty;
+            FigBytesUtility.Core.Utility utility = new FigBytesUtility.Core.Utility(strVaultURL, strVaultAppReaderName);
+            strDecryptText = utility.Decryption(strText);
+            return strDecryptText;
+        }
+    }
+}
