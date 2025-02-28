876b95946fc2f78c787ce78744d0f35bb5925a87
Jay Nayon <Jay.Nayon@amcsgroup.com>
2025-02-28 13:49:25 +0800

    Change return string to status code; id returned is neither report's/model's

--- PowerBIAutomationApp/UploadFunctions.cs
+++ PowerBIAutomationApp/UploadFunctions.cs
@@ -108,19 +108,7 @@ namespace PowerBIAutomationApp
                                 _logger.LogError($"Failed to upload model. Status Code: {response.StatusCode}, Response: {errorResponse}");
                             }
 
-                            //return response.StatusCode.ToString();
-                            var jsonBody = await response.Content.ReadAsStringAsync();
-                            using (JsonDocument doc = JsonDocument.Parse(jsonBody))
-                            {
-                                if (doc.RootElement.TryGetProperty("id", out JsonElement idElement))
-                                {
-                                    return idElement.GetString() ?? throw new Exception("Failed to retrieve uploaded report ID.");
-                                }
-                                else
-                                {
-                                    throw new Exception("Response JSON does not contain 'id'.");
-                                }
-                            }
+                            return response.StatusCode.ToString();
                         }
                     }
                 }
